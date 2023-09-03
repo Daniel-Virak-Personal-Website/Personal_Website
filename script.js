a0135e90bea3fc1b0a0d6b6cea65719cb19f6a26
dvirak <dvirak@vt.edu>
2023-09-03 12:45:30 -0400

    First attempt at adding css and js

---
+++ script.js
@@ -0,0 +1,25 @@
+// function home(){
+//   const name = window.prompt("What is your name?");
+// }
+
+function index() {
+  const name = window.prompt("What is your name?");
+  alert(`Thanks for visiting my website, ${name}! I hope you enjoy!`);
+  localStorage.setItem("userName", name);
+}
+
+function about() {
+  alert(
+    `Thanks for visiting my about page, ${localStorage.getItem(
+      "userName"
+    )}! I hope you enjoy!`
+  );
+}
+
+function portfolio() {
+  alert(
+    `Thanks for visiting my portfolio page, ${localStorage.getItem(
+      "userName"
+    )}! It's quite empty now but we will build it up soon!`
+  );
+}
